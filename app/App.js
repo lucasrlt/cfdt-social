7f64931853b4bb6954e7a60385a8aef62b5b6106
Lucas Rollet <lucas.rollet69@gmail.com>
2021-09-16 14:49:11 +0200

    Fix ios prod issues

--- app/App.js
+++ app/App.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {Alert, Platform, StatusBar, Text, View} from 'react-native';
+import {Alert, Platform, StatusBar, Text, View, SafeAreaView} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import AuthProvider from './src/context/AuthProvider';
 import PostsProvider from './src/context/PostsProvider';
@@ -8,19 +8,18 @@ import * as Notifications from 'expo-notifications';
 import * as SplashScreen from 'expo-splash-screen';
 import {Button} from './src/components/Button';
 
-export default function App() {
-  useEffect(() => {
-    (async () => {
-      await SplashScreen.preventAutoHideAsync();
-    })();
-  }, []);
+SplashScreen.preventAutoHideAsync().catch(() => {});
 
+
+export default function App() {
   return (
-    <SafeAreaProvider>
+    // <SafeAreaProvider>
       <AuthProvider>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "red" }}>
         <StatusBar barStyle="light-content" />
         <Navigation />
+        </SafeAreaView>
       </AuthProvider>
-    </SafeAreaProvider>
+    // </SafeAreaProvider>
   );
 }
